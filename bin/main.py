4c3a97b6d8803444f0e7dc5fe2e7e6f23dc0ce83
Richard Harris <richard.w.harris@mac.com>
2017-11-06 20:32:25 +0000

    refactoring code to util

--- bin/main.py
+++ bin/main.py
@@ -3,25 +3,6 @@ from markov_football.util import *
 from pprint import pprint
 from markov_football.name import football_clubs_by_league
 
-
-def display_league(lineups_by_name: Dict[str, List[Selection]]):
-    table = create_next_goal_matrix(lineups_by_name.values(), team_states=[TeamState.WITH_M])
-    mean_table = table.loc[:, ['mean']]
-
-    player_counts_by_position_list = [
-        {position: len(players)
-         for position, players in lineups_by_name[lineup_name].formation().items()}
-        for lineup_name in mean_table.index
-    ]
-
-    for position in Position:
-        mean_table[position.name] = pd.Series([player_counts_by_position[position]
-                                               for player_counts_by_position in
-                                               player_counts_by_position_list], index=mean_table.index)
-
-    print(mean_table)
-
-
 if __name__ == '__main__':
 
     clubs_by_league = football_clubs_by_league()
@@ -37,51 +18,14 @@ if __name__ == '__main__':
 
         points, wins, draws, losses, goals, conceded_goals = Counter(), Counter(), Counter(), Counter(), Counter(), Counter()
 
+        player_position_history = defaultdict(list)
+
         for week, fixtures_this_week in enumerate(fixtures(selections_by_name.keys())):
+            hold_week(fixtures=fixtures_this_week, selections_by_name=selections_by_name,
+                      player_position_history=player_position_history, goals=goals, conceded_goals=conceded_goals,
+                      points=points, wins=wins, losses=losses, draws=draws)
 
-            for club_1, club_2 in fixtures_this_week:
-                print('Week %d: %s vs. %s' % (week, club_1, club_2))
 
-                if not club_1 or not club_2:
-                    continue
-
-                selection_1 = selections_by_name[club_1]
-                selection_2 = selections_by_name[club_2]
-
-                selection_1, selection_2 = optmise_player_positions_in_parrallel(
-                    selections=(selection_1, selection_2),
-                    team_states=[TeamState.WITH_M])
-
-                selections_by_name[club_1] = selection_1
-                selections_by_name[club_2] = selection_2
-
-                display_league(lineups_by_name={club_1: selection_1,
-                                                club_2: selection_2})
-
-                score_keeper = hold_fixture(selection_1=selection_1, selection_2=selection_2)
-
-                goals.update(score_keeper)
-                conceded_goals[club_1] += score_keeper[club_2]
-                conceded_goals[club_2] += score_keeper[club_1]
-
-                if score_keeper[club_1] > score_keeper[club_2]:
-                    points[club_1] += 3
-                    wins[club_1] += 1
-                    losses[club_2] += 1
-                elif score_keeper[club_2] > score_keeper[club_1]:
-                    points[club_2] += 3
-                    wins[club_2] += 1
-                    losses[club_1] += 1
-                else:
-                    points[club_1] += 1
-                    points[club_2] += 1
-                    draws[club_1] += 1
-                    draws[club_2] += 1
-
-                print()
-                print('%s: %d\t%s: %d' % (club_1, score_keeper[club_1], club_2, score_keeper[club_2]))
-                print()
-                print()
 
             data = OrderedDict([('p', [points[name] for name in names]),
                                 ('w', [wins[name] for name in names]),
@@ -96,3 +40,13 @@ if __name__ == '__main__':
             print('Table after week %d.' % week)
             print(table)
             print()
+
+        for club, selection in selections_by_name.items():
+            print(club)
+            for player in selection.keys():
+                count = Counter(player_position_history[player.name])
+                print(player.name, [(position.name, count[position])
+                                    for position in Position])
+            print()
+
+        break
--- markov_football/util.py
+++ markov_football/util.py
@@ -187,3 +187,73 @@ def hold_fixture(selection_1: Selection, selection_2: Selection):
             s = next_s
 
     return score_keeper
+
+
+def display_league(lineups_by_name: Dict[str, List[Selection]]):
+    table = create_next_goal_matrix(lineups_by_name.values(), team_states=[TeamState.WITH_M])
+    mean_table = table.loc[:, ['mean']]
+
+    player_counts_by_position_list = [
+        {position: len(players)
+         for position, players in lineups_by_name[lineup_name].formation().items()}
+        for lineup_name in mean_table.index
+    ]
+
+    for position in Position:
+        mean_table[position.name] = pd.Series([player_counts_by_position[position]
+                                               for player_counts_by_position in
+                                               player_counts_by_position_list], index=mean_table.index)
+
+    print(mean_table)
+
+
+def hold_week(fixtures: List[Tuple[str, str]], selections_by_name: Dict[str, Selection],
+              player_position_history: Dict[str, List[Position]], goals: Counter, conceded_goals: Counter,
+              points: Counter, wins: Counter, losses: Counter, draws: Counter):
+    for club_1, club_2 in fixtures:
+        print('%s vs. %s' % (club_1, club_2))
+
+        if not club_1 or not club_2:
+            continue
+
+        selection_1 = selections_by_name[club_1]
+        selection_2 = selections_by_name[club_2]
+
+        selection_1, selection_2 = optmise_player_positions_in_parrallel(
+            selections=(selection_1, selection_2),
+            team_states=[TeamState.WITH_M])
+
+        for selection in [selection_1, selection_2]:
+            for player, position in selection.items():
+                player_position_history[player.name].append(position)
+
+        selections_by_name[club_1] = selection_1
+        selections_by_name[club_2] = selection_2
+
+        display_league(lineups_by_name={club_1: selection_1,
+                                        club_2: selection_2})
+
+        score_keeper = hold_fixture(selection_1=selection_1, selection_2=selection_2)
+
+        goals.update(score_keeper)
+        conceded_goals[club_1] += score_keeper[club_2]
+        conceded_goals[club_2] += score_keeper[club_1]
+
+        if score_keeper[club_1] > score_keeper[club_2]:
+            points[club_1] += 3
+            wins[club_1] += 1
+            losses[club_2] += 1
+        elif score_keeper[club_2] > score_keeper[club_1]:
+            points[club_2] += 3
+            wins[club_2] += 1
+            losses[club_1] += 1
+        else:
+            points[club_1] += 1
+            points[club_2] += 1
+            draws[club_1] += 1
+            draws[club_2] += 1
+
+        print()
+        print('%s: %d\t%s: %d' % (club_1, score_keeper[club_1], club_2, score_keeper[club_2]))
+        print()
+        print()
